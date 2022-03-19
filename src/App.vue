b1631bd3d8e698fee6156deb105bd2770b6f9854
19990710simplelife <1753848616@qq.com>
2022-03-19 17:03:18 +0800

    环境搭建

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
+<!--
+ * @Author: your name
+ * @Date: 2022-03-19 16:55:15
+ * @LastEditTime: 2022-03-19 16:59:48
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \tabbar\src\App.vue
+-->
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-19 16:55:57
+ * @LastEditTime: 2022-03-19 16:55:57
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \tabbar\vue.config.js
+ */
+module.exports = {
+    devServer: {
+        port: 3000,
+        open: true
+    },
+    lintOnSave: false
+}
